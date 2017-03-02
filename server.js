56e519118b3aabd38b5ffa0bc38112a88db0cd07
kaloygabriento <gabrientocarloit1a@gmail.com>
2017-03-02 14:34:47 +0800

    kaloy

---
+++ server.js
@@ -0,0 +1,19 @@
+//server.js
+//require modules
+var express = require('express');
+var path = require('path');
+//instantiate express
+var app = express();
+//set port
+app.set('port', (process.env.PORT || 5000));
+//use static files
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/js', express.static(__dirname + '/js'));
+//express routes
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+//express server listen
+var server = app.listen(app.get('port'), function(){
+  console.log('Server listening on port ',app.get('port'));
+});
